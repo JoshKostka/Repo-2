51d9b4a5b2e73c3ec58be764ea599b13062c2d20
JoshKostka <p460842@tafe.wa.edu.au>
2022-02-07 11:15:44 +0800

    no message

---
+++ Repositories/Coding/Python Coding/Pythonlearning.py
@@ -0,0 +1,5 @@
+user_input = input("Enter a phrase: ") #Prompts the user for input
+print("Your phrase is " + user_input) #This is an example of string concatonation
+
+user_input = user_input.replace("agression", "aggression") #This replaces a string
+print(user_input) #Corrected string
